34866b4da08221e50e849557497309913563bf04
oneoneoneoneoneoneone <oneoneoneoneoneoneone>
2023-02-23 01:36:39 +0900

    통신 코드 정리

--- NaverWebtoonCloneCoding/Network/LocalNetwork.swift
+++ NaverWebtoonCloneCoding/Network/LocalNetwork.swift
@@ -8,106 +8,101 @@
 import Foundation
 import UIKit
 import SnapKit
-import SwiftUI
 
 class LocalNetwork{
-    private let session: URLSession
     let api = LocalAPI()
     
-    init(session: URLSession = .shared){
-        self.session = session
-    }
-    
-    //var dataTasks = [URLSessionDataTask]()
-    //var bookList = [Book]()
-    var itemList = [Item]()
-    var query = ""
-    
-    func getLocation(by querys: [String]) -> [Item]{
+    func getLocation(id: String, by querys: [String]) -> [Item]{
+        var items: [Item] = []
         
         for query in querys{
-            guard let url = api.getLocation(path: "", by: query).url else {
+            guard let url = api.getLocation(path: id, by: query).url else {
                 print("ERROR")
-                return itemList
+                return items
             }
-
+            
+            //리퀘스트 생성
             var request = URLRequest(url: url)
             request.httpMethod = "GET"
-            request.setValue("hpNnarQFrSfzCnJzSUH2", forHTTPHeaderField: "X-Naver-Client-Id")
-            request.setValue("DUf7UK6QHq", forHTTPHeaderField: "X-Naver-Client-Secret")
+            request.setValue(APIKey().X_Naver_Client_Id, forHTTPHeaderField: "X-Naver-Client-Id")
+            request.setValue(APIKey().X_Naver_Client_Secret, forHTTPHeaderField: "X-Naver-Client-Secret")
             
-            
-//            AF.request(url, method: .get, parameters: ["query":query], encoding: URLEncoding.default, headers: ["X-Naver-Client-Id": "hpNnarQFrSfzCnJzSUH2", "X-Naver-Client-Secret":"DUf7UK6QHq"])
-//                .validate(statusCode: 200..<300)
-//                .responseDecodable(of: Books.self){ response in
-//                switch response.result{
-//                case .success:
-//                    let books = response.value //else {return}//try? JSONDecoder().decode([Books].self, from: response.data!)
-//                case .failure:
-//                    print("Error")
-//                }
-//            }
-        //}
-            
-            
+            sleep(1)
+            //태스크 생성
             URLSession.shared.dataTask(with: request){[weak self] data, response, error in
-                guard error == nil,
-                      let self = self else{
-                    print("ERROR")
-                    return
-                }
-                guard let response = response as? HTTPURLResponse else{
-                    print("ERROR")
-                    return
-                }
-                guard let data = data,
-                      let books = try? JSONDecoder().decode([Books].self, from: data)
-                else{
-                    print("ERROR")
+                do{
+                    guard error == nil,
+                          let self = self else{
+                        print("ERROR")
+                        return
+                    }
+                    guard let response = response as? HTTPURLResponse else{
+                        print("response ERROR")
+                        return
+                    }
+                    guard let data = data else{
+                        print("response ERROR")
+                        return
+                    }
+                    
+                    switch response.statusCode{
+                    case(200...299) :
+                        do {
+                            let books = try JSONDecoder().decode(Books.self, from: data)
+                            guard let book = books.items.first else{ return }
+                            
+                            var item = Item()
+                            
+                            item.id = Bool.random()
+                            item.title = book.title
+                            item.description = book.description
+                            item.image = book.image
+                            item.author = book.author
+                            item.link = book.link
+                            item.pubdate = book.pubdate
+                            item.publisher = book.publisher
+                            item.isbn = book.isbn
+                            item.updateDay = ["월", "화", "수", "목", "금", "토", "일"].randomElement() ?? "월"
+                            item.score = round(Double.random(in: 0...10) * 100)/100
+                            item.like = Int.random(in: 0...999999)
+                            item.isUpdate = Bool.random()
+                            item.isStop = Bool.random()
+                            item.isIncrease = Bool.random()
+                            
+                            items.append(item)
+                        }
+                    case(400...499):
+                        let error = try JSONDecoder().decode(Error.self, from: data)
+                        print("""
+                            ERROR: Client ERROR \(response.statusCode)
+                            ERROR Message: \(error.errorMessage)
+                            """)
+                        return
+                    case(500...599):
+                        _ = try JSONDecoder().decode(Error.self, from: data)
+                        print("""
+                            ERROR: Server ERROR \(response.statusCode)
+                            Response: \(response)
+                            """)
+                        return
+                    default:
+                        _ = try JSONDecoder().decode(Error.self, from: data)
+                        print("""
+                            ERROR: \(response.statusCode)
+                            Response: \(response)
+                            """)
+                        return
+                    }
+                }catch(let error) {
+                    print("URLSession Error - \(error.localizedDescription)")
                     return
                 }
                 
-                switch response.statusCode{
-                case(200...299) : do {
-                    let book = books.first!
-
-                    var item: Item!
-
-                    item.title = book.items.first!.title
-                    item.description = book.items.first!.description
-                    item.image = book.items.first!.image
-                    item.author = book.items.first!.author
-                    item.link = book.items.first!.link
-                    item.pubdate = book.items.first!.pubdate
-                    item.isbn = book.items.first!.isbn
-                    item.updateDay = ""
-                    item.score = 9.99
-                    item.isUpdate = false
-                    item.isStop = true
-                    item.isIncrease = true
-
-                    self.itemList.append(item)
-                }
-                case(400...499):
-                    print("""
-                        ERROR: Client ERROR \(response.statusCode)
-                        Response: \(response)
-                        """)
-                case(500...599):
-                    print("""
-                        ERROR: Server ERROR \(response.statusCode)
-                        Response: \(response)
-                        """)
-                default:
-                    print("""
-                        ERROR: \(response.statusCode)
-                        Response: \(response)
-                        """)
-                }
-                
-            }.resume()
-            
+            }
+            //실행
+            .resume()
         }
-        return itemList
+        
+        return items
     }
 }
